f758dc4c7959a65521fd6d1ed6224c937c2509ed
Juan Sebastian Valdés Sanche - Ceiba Software <sebastian.valdes@ceiba.com.co>
2021-03-02 12:18:41 -0500

    v3.5.0

--- BackEnd/test/unitarias/dominio/orden/orden.spec.ts
+++ BackEnd/test/unitarias/dominio/orden/orden.spec.ts
@@ -1,18 +1,32 @@
 import { ErrorHorarioLaboral } from 'src/dominio/errores/error-horario-laboral';
-import { OrdenTestDataBuilder } from '../../../tdb/orden/orden-tdb'
+import { ErrorFranjaHoraria } from 'src/dominio/errores/error-franja-horaria';
+import { OrdenTestDataBuilder } from '../../../tdb/orden/orden-tdb';
+
 describe('Orden', () => {
+  it('orden con horario fuera del horario laboral deberia retornar error', () => {
+    return expect(async () =>
+      new OrdenTestDataBuilder().withHoraEntrega('18:00:00').build(),
+    ).rejects.toStrictEqual(
+      new ErrorHorarioLaboral(
+        'La hora ingresada no corresponde al horario laboral',
+      ),
+    );
+  });
 
-  it('orden con horario fuera del horario laboral retornar error', () => {
-
-    return expect(async () => new OrdenTestDataBuilder().withHoraEntrega('18:00:00').build())
-      .rejects
-      .toStrictEqual(new ErrorHorarioLaboral('La hora ingresada no corresponde al horario laboral'));
+  it('orden con franja horaria deberia retornar error', () => {
+    return expect(async () =>
+      new OrdenTestDataBuilder().withHoraEntrega('12:21:00').build(),
+    ).rejects.toStrictEqual(
+      new ErrorFranjaHoraria(
+        'La franja horaria determina no es valida',
+      ),
+    );
   });
 
   it('orden con horario laboral debería crear bien', () => {
-
-    const orden = new OrdenTestDataBuilder().withHoraEntrega('14:00:00').build();
+    const orden = new OrdenTestDataBuilder()
+      .withHoraEntrega('14:00:00')
+      .build();
     expect(orden.idCoordinador).toEqual(2);
-
   });
 });
